717e526efc3ee80212839a91fbc36fae0c7fe16f
ZachRober <nolethedgehogdie@gmail.com>
2024-08-30 12:09:05 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -6,6 +6,14 @@ console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
 
+//Task 2: Compound Data Types
+let products = ["Milk", "Fruit Loops", "Ground Beef"];
+const productDetails = {
+    name1:products, 
+    price:10.99, 
+    stock:true, 
+    
+}
 
-
+console.log(products, productDetails);
 
