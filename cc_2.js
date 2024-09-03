e97434cae09f0c231214d7227af65ac345645839
ZachRober <nolethedgehogdie@gmail.com>
2024-09-03 15:48:29 -0400

    Create and test calculate tip function

--- cc_2.js
+++ cc_2.js
@@ -5,6 +5,12 @@ let tip = (50 <= bill <= 300) ? (0.15*bill) : (0.2*bill);
 let total = (tip+bill);
 console.log("The bill was $"+ bill, "the tip was $"+ tip, "and the total value was $"+ total);//display all 3 calcs
 
+function calculateTip(x) {
+if(50<=x<=300) return (x*0.15);//create if else statmenet for conditionals
+else return (x*0.20);
+}
+console.log(calculateTip(100));
+
 
 
 
