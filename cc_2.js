83b3cf3e383b01574ccc409f1d8b9e1a53dfb97c
ZachRober <nolethedgehogdie@gmail.com>
2024-09-03 17:35:07 -0400

    Implement and populate arrays for bills, tips, and totals

--- cc_2.js
+++ cc_2.js
@@ -14,7 +14,11 @@ else {return x*0.2}//if else statement to make conditional
 
 console.log(calculateTip(100));//output 15
 
+let bills = [275,40,430,125,555,44];
+let tips = bills.map(x => calculateTip(x));//applies function to every number in bills and makes new array with outputs
+let totals = bills.map(x => x + calculateTip(x));//same as above but adds tips array to bills array
 
+console.log(tips, totals);
 
 
 
