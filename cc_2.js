03faf5468636b3b569d3831b598185cb6064e443
ZachRober <nolethedgehogdie@gmail.com>
2024-09-03 15:35:07 -0400

    Add output functionality to display bill, tip, and total

--- cc_2.js
+++ cc_2.js
@@ -2,7 +2,8 @@
 let bill = 250;
 let tip = (50 <= bill <= 300) ? (0.15*bill) : (0.2*bill);
 
-
+let total = (tip+bill);
+console.log("The bill was $"+ bill, "the tip was $"+ tip, "and the total value was $"+ total);//display all 3 calcs
 
 
 
